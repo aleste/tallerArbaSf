5228ce1911fb310899edf2b885ed138ce836ca9b
Alejandro Steinmetz <asteinmetz78@gmail.com>
2017-10-31 20:07:07 -0300

    Crea entidades del modelo

---
+++ src/AppBundle/Entity/Origen.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Origen
+ *
+ * @ORM\Table(name="origen")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\OrigenRepository")
+ */
+class Origen
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=30)
+     */
+    private $nombre;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Origen
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
+
